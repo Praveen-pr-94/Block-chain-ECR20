a5619111c7990156b56608107480c636c6d9241f
praveen.pr <pr.praveen195@gmail.com>
2021-05-22 23:56:00 +0530

    transfer added

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,9 +5,27 @@ contract DappToken {
     string public name= "DappToken";
     string public symbol= "Dapp";
     string public statndard= "Dapp token v1.0";
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
     mapping(address => uint) public balanceOf;
+
     constructor(uint _intialSupply ) public{
         balanceOf[msg.sender] = _intialSupply;
         totalSupply = _intialSupply;
+    }
+
+    // transfer from one account to another
+    function transfer(address _to , uint _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender]-= _value;
+        balanceOf[_to]+= _value;
+
+        emit Transfer(msg.sender , _to , _value);
+        return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -22,6 +22,45 @@ contract('DappToken', (accounts) => {
         }).then((adminBalance) => {
                 assert.equal(adminBalance.toNumber() , 10 , 'it shobe be 10 not to be 0' )
         })  
+    })
+
+    // it('transfer ownership',()=> {
+    //         return DappToken.deployed().then(instance => {
+    //             tokenInstance = instance;
+    //             return tokenInstance.transfer(accounts[1], 100);
+    //         }).then(assert.fail).catch(err=> {
+    //             assert(err.message.indexOf('revert') >= 0, 'error message contain revert');
+    //             return tokenInstance.transfer(accounts[1], 5);
+    //         }).then((recipiet) => {
+    //             console.log(recipiet)
+    //              return tokenInstance.balanceOf(accounts[1]);
+    //         }).then((balance1) => {
+    //             assert.equal(balance1.toNumber() , 5, 'adds the amount to reciving account');
+    //             return tokenInstance.balanceOf(accounts[0]);
+    //        }).then((balance0) => {
+    //             assert.equal(balance0.toNumber() , 5, 'amount deducted from sending account');
+    //        })
+    // })
+
+    it('transfer ownership',async()=> {
+        const instance = await DappToken.deployed();
+        try{
+            await instance.transfer.call(accounts[1], 100, {from: accounts[0]});
+        }catch(err){
+            assert(err.message.indexOf('revert') >= 0, 'error message contain revert');
+        }
+        const success = await instance.transfer.call(accounts[1], 1, {from: accounts[0]});
+        assert.equal(success , true , 'it must be true')
+        const receipt = await instance.transfer(accounts[1], 5);
+        await assert.equal(receipt.logs.length , 1, 'trigger one event');
+        await assert.equal(receipt.logs[0].event,'Transfer' , 'Should be Transfer');
+        await assert.equal(receipt.logs[0].args._from,accounts[0] , 'Match from account');
+        await assert.equal(receipt.logs[0].args._to,accounts[1] , 'Match to account');
+        await assert.equal(receipt.logs[0].args._value, 5, 'Match transfer amount');
+        const balance1 = await instance.balanceOf(accounts[1]);
+        assert.equal(balance1.toNumber() , 5, 'adds the amount to reciving account');
+        const balance0 = await instance.balanceOf(accounts[0]);
+        assert.equal(balance0.toNumber() , 5, 'amount deducted from sending account');
 
     })
 })
