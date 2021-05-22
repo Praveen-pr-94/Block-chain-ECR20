d753490ae71aa76e6d63b0a1a7d47290e2c43197
praveen.pr <pr.praveen195@gmail.com>
2021-05-23 01:12:12 +0530

    approval added

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -12,20 +12,32 @@ contract DappToken {
         uint _value
     );
 
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
     mapping(address => uint) public balanceOf;
+
+    mapping(address => mapping(address => uint)) public allowance;
 
     constructor(uint _intialSupply ) public{
         balanceOf[msg.sender] = _intialSupply;
         totalSupply = _intialSupply;
     }
 
-    // transfer from one account to another
     function transfer(address _to , uint _value) public returns (bool success){
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender]-= _value;
         balanceOf[_to]+= _value;
-
         emit Transfer(msg.sender , _to , _value);
+        return true;
+    }
+
+    function approve(address _spender , uint _value) public returns (bool success){
+        allowance[msg.sender][_spender] =_value;
+        emit Approve(msg.sender, _spender ,_value);
         return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,6 +1,7 @@
 const DappToken = artifacts.require('./DappToken.sol');
 let tokenInstance;
 contract('DappToken', (accounts) => {
+
     it('initialise contract with correct value', async() => {
         const instance = await DappToken.deployed();
         const name = await instance.name();
@@ -12,35 +13,14 @@ contract('DappToken', (accounts) => {
     })
 
 
-    it('it allocates initial supply on deployment' , () => {
-        return DappToken.deployed().then((instance)=>{
-            tokenInstance = instance;
-            return tokenInstance.totalSupply();
-        }).then((data) => {
-            assert.equal(data.toNumber() , 10 , 'set total supplt to 1M');
-            return tokenInstance.balanceOf(accounts[0]);
-        }).then((adminBalance) => {
-                assert.equal(adminBalance.toNumber() , 10 , 'it shobe be 10 not to be 0' )
-        })  
+    it('it allocates initial supply on deployment' , async() => {
+        const instance = await DappToken.deployed();
+        const data = await instance.totalSupply();
+        assert.equal(data.toNumber() , 10 , 'set total supplt to 1M');
+        const adminBalance = await instance.balanceOf(accounts[0]);
+        assert.equal(adminBalance.toNumber() , 10 , 'it shobe be 10 not to be 0' );
     })
 
-    // it('transfer ownership',()=> {
-    //         return DappToken.deployed().then(instance => {
-    //             tokenInstance = instance;
-    //             return tokenInstance.transfer(accounts[1], 100);
-    //         }).then(assert.fail).catch(err=> {
-    //             assert(err.message.indexOf('revert') >= 0, 'error message contain revert');
-    //             return tokenInstance.transfer(accounts[1], 5);
-    //         }).then((recipiet) => {
-    //             console.log(recipiet)
-    //              return tokenInstance.balanceOf(accounts[1]);
-    //         }).then((balance1) => {
-    //             assert.equal(balance1.toNumber() , 5, 'adds the amount to reciving account');
-    //             return tokenInstance.balanceOf(accounts[0]);
-    //        }).then((balance0) => {
-    //             assert.equal(balance0.toNumber() , 5, 'amount deducted from sending account');
-    //        })
-    // })
 
     it('transfer ownership',async()=> {
         const instance = await DappToken.deployed();
@@ -52,15 +32,33 @@ contract('DappToken', (accounts) => {
         const success = await instance.transfer.call(accounts[1], 1, {from: accounts[0]});
         assert.equal(success , true , 'it must be true')
         const receipt = await instance.transfer(accounts[1], 5);
-        await assert.equal(receipt.logs.length , 1, 'trigger one event');
-        await assert.equal(receipt.logs[0].event,'Transfer' , 'Should be Transfer');
-        await assert.equal(receipt.logs[0].args._from,accounts[0] , 'Match from account');
-        await assert.equal(receipt.logs[0].args._to,accounts[1] , 'Match to account');
-        await assert.equal(receipt.logs[0].args._value, 5, 'Match transfer amount');
+        assert.equal(receipt.logs.length , 1, 'trigger one event');
+        assert.equal(receipt.logs[0].event,'Transfer' , 'Should be Transfer');
+        assert.equal(receipt.logs[0].args._from,accounts[0] , 'Match from account');
+        assert.equal(receipt.logs[0].args._to,accounts[1] , 'Match to account');
+        assert.equal(receipt.logs[0].args._value, 5, 'Match transfer amount');
         const balance1 = await instance.balanceOf(accounts[1]);
         assert.equal(balance1.toNumber() , 5, 'adds the amount to reciving account');
         const balance0 = await instance.balanceOf(accounts[0]);
         assert.equal(balance0.toNumber() , 5, 'amount deducted from sending account');
+    })
 
+    it('Approve tokens from delegated transfer' , async() => {
+        const instance = await DappToken.deployed();
+        // try{
+        //     await instance.approve.call(accounts[1], 100, {from: accounts[0]});
+        // }catch(err){
+        //     assert(err.message.indexOf('revert') >= 0, 'error message contain revert');
+        // }
+        const success = await instance.approve.call(accounts[1], 100 ,{from: accounts[0]});
+        assert.equal(success , true , 'it must be true');
+        const receipt = await instance.approve(accounts[1], 5);
+        assert.equal(receipt.logs.length , 1, 'trigger one event');
+        assert.equal(receipt.logs[0].event,'Approve' , 'Should be Approve');
+        assert.equal(receipt.logs[0].args._owner,accounts[0] , 'Match from account');
+        assert.equal(receipt.logs[0].args._spender,accounts[1] , 'Match to account');
+        assert.equal(receipt.logs[0].args._value, 5, 'Match amount');
+        const allowance = await instance.allowance(accounts[0], accounts[1]);
+        assert.equal(allowance.toNumber() , 5, 'store allowance in delegated transfer');
     })
 })
