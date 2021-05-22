f58bf59515fd2e9a7cad1f5001782a1ba04d9be0
praveen.pr <pr.praveen195@gmail.com>
2021-05-22 20:43:01 +0530

    ERC token created

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.5.16;
 
 contract DappToken {
     uint public totalSupply;
+    string public name= "DappToken";
+    string public symbol= "Dapp";
+    string public statndard= "Dapp token v1.0";
+    mapping(address => uint) public balanceOf;
     constructor(uint _intialSupply ) public{
+        balanceOf[msg.sender] = _intialSupply;
         totalSupply = _intialSupply;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,12 +1,27 @@
 const DappToken = artifacts.require('./DappToken.sol');
+let tokenInstance;
 contract('DappToken', (accounts) => {
-    it('set total supply on deployment' , () => {
+    it('initialise contract with correct value', async() => {
+        const instance = await DappToken.deployed();
+        const name = await instance.name();
+        const symbol = await instance.symbol();
+        const statndard = await instance.statndard();
+        assert.equal(name , "DappToken" , "name must be DappToken");
+        assert.equal(symbol , "Dapp" , "symbol must be Dapp");
+        assert.equal(statndard , "Dapp token v1.0" , "statndard must be Dapp token v1.0");
+    })
+
+
+    it('it allocates initial supply on deployment' , () => {
         return DappToken.deployed().then((instance)=>{
-            totalInstance = instance;
-            return totalInstance.totalSupply();
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
         }).then((data) => {
-            assert.equal(data.toNumber() , 10 , 'set total supplt to 1M')
-        })
+            assert.equal(data.toNumber() , 10 , 'set total supplt to 1M');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then((adminBalance) => {
+                assert.equal(adminBalance.toNumber() , 10 , 'it shobe be 10 not to be 0' )
+        })  
 
     })
 })
